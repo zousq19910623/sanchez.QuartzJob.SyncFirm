06f643f95ff09665dfeacda2eab9451b0cf1ede7
sanchez.zou <sanchez.zou@hirede.com>
2018-07-04 15:25:13 +0800

    add quartz package and hosted service

---
+++ SyncFirmToTbd/SyncFirmHostedService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SyncFirmToTbd
+{
+    public class SyncFirmHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger m_Logger;
+
+        public SyncFirmHostedService(ILogger<SyncFirmHostedService> logger)
+        {
+            m_Logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            m_Logger.LogInformation("Timed Background Service is starting.");
+
+
+
+            m_Logger.LogInformation("Timed Background Service is started.");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            m_Logger.LogInformation("Timed Background Service is stopping.");
+
+
+
+            m_Logger.LogInformation("Timed Background Service is stopped.");
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

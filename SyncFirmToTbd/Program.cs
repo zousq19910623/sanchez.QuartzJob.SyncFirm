0c359280dd77c19caef0c4d37364de0cc6056223
sanchez.zou <sanchez.zou@hirede.com>
2018-07-04 14:31:12 +0800

    init project

---
+++ SyncFirmToTbd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncFirmToTbd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
